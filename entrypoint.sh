7e65cb7a3efb39dadb63a2f47eca86eb44476556
smk5797 <smk579702@naver.com>
2024-04-19 08:54:19 +0900

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -56,10 +56,10 @@ echo "[/api/v1/scan] Start the scan"
 curl -X POST --url ${MOBSF_URL}/api/v1/scan --data "scan_type=${SCAN_TYPE}&file_name=${FILE_NAME}&hash=${HASH}" -H "Authorization:${MOBSF_API_KEY}"
 echo "[/api/v1/scan] Scan finisehd"
 
-# Generate the json report.
-echo "[/api/v1/report_json] Generate the json report"
-curl -X POST --url ${MOBSF_URL}/api/v1/report_json --data "hash=${HASH}" -H "Authorization:${MOBSF_API_KEY}" --output ${OUTPUT_FILE_NAME}.json
-echo "[/api/v1/report_json] JSON report generated"
+# # Generate the json report.
+# echo "[/api/v1/report_json] Generate the json report"
+# curl -X POST --url ${MOBSF_URL}/api/v1/report_json --data "hash=${HASH}" -H "Authorization:${MOBSF_API_KEY}" --output ${OUTPUT_FILE_NAME}.json
+# echo "[/api/v1/report_json] JSON report generated"
 
 # Generate the pdf report.
 echo "[/api/v1/download_pdf] Generate the PDF report"
@@ -67,6 +67,6 @@ curl -X POST --url ${MOBSF_URL}/api/v1/download_pdf --data "hash=${HASH}" -H "Au
 echo "[/api/v1/download_pdf] PDF report generated"
 
 # Remove analysis from MobSF server.
-echo "[/api/v1/delete_scan] Remove analysis from MobSF server"
-curl -X POST --url ${MOBSF_URL}/api/v1/delete_scan --data "hash=${HASH}" -H "Authorization:${MOBSF_API_KEY}"
-echo "[/api/v1/delete_scan] Analysis removed"
+# echo "[/api/v1/delete_scan] Remove analysis from MobSF server"
+# curl -X POST --url ${MOBSF_URL}/api/v1/delete_scan --data "hash=${HASH}" -H "Authorization:${MOBSF_API_KEY}"
+# echo "[/api/v1/delete_scan] Analysis removed"
